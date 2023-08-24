c61285e96ae770aced88d463768c033e741493f4
Arthath Agary <arthathagary@gmail.com>
2023-08-24 20:21:32 +0530

    City name and map changed

--- src/components/HeroSection/WeatherCard.jsx
+++ src/components/HeroSection/WeatherCard.jsx
@@ -18,7 +18,7 @@ const WeatherCard = () => {
     const getTemperature = async () => {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=Oshawa&appid=21b1921fce82ffcdf76134de36476f3d&units=metric`
+          `https://api.openweathermap.org/data/2.5/weather?q=Markham&appid=21b1921fce82ffcdf76134de36476f3d&units=metric`
         );
 
         // Extract temperature from the API response and update state
@@ -38,7 +38,7 @@ const WeatherCard = () => {
         <div className='bg-white shadow-lg sm:rounded-3xl sm:p-8 bg-clip-padding bg-opacity-60 border border-gray-200 backdrop-blur-none'          >
             <div className="mb-2 md:text-2xl font-bold tracking-tight text-gray-900  py-12 px-6 text-center">
             <h3 className='mb-3 text-xl font-normal'>{new Date().toLocaleDateString('en-US', options)}</h3>
-            <h3 className="mb-3 text-xl font-normal">Oshawa, Ontario, Canada</h3>
+            <h3 className="mb-3 text-xl font-normal">Markham, Ontario, Canada</h3>
             
             <div className="flex justify-center"><Image className="mb-3" src={sunGif} width={'100px'} height={'100px'} alt=""/></div>
             {temperature ? <h3>{`${temperature} °C`}</h3> : <h3>Loading...</h3>}
